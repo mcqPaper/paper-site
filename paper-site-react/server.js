610b74e71fb34cdfa6268bb912065266e52d4401
ccnextgen <103283908+ccnextgen@users.noreply.github.com>
2022-11-18 23:53:47 +0530

    docker

--- paper-site-react/server.js
+++ paper-site-react/server.js
@@ -9,7 +9,9 @@ app.use(compression());
 app.use(express.static(__dirname + '/build/'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/build/index.html'));
+    console.log(`start listening on port: ${port}`)
 });
 
 
 http.createServer(options, app).listen(port);
+console.log(`start listening on port: ${port}`)
